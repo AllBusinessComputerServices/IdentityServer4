cd8c0597591504ce43b3ce1307fded2a8c06b4b6
William Maintz II <wmmaintz@gmail.com>
2018-07-13 09:31:22 -0500

    Add project files.

---
+++ LaunchServer.cmd
@@ -0,0 +1,9 @@
+@ECHO OFF
+CLS
+SET CMDSTR="CLS & DOTNET run --project ./src/MyIdentityServer/MyIdentityServer.csproj & ECHO. & PAUSE"
+
+ECHO.
+ECHO  Launching IdentityServer4 in separate cmd window ...
+ECHO.
+START CMD /C %CMDSTR%
+SET CMDSTR=
---
+++ LaunchTests.cmd
@@ -0,0 +1,9 @@
+@ECHO OFF
+CLS
+SET CMDSTR="CLS & DOTNET test ./tests/MyApiServerTests/MyApiServerTests.csproj & ECHO. & PAUSE"
+
+ECHO.
+ECHO  Launching IdentityServer4 tests in separate cmd window ...
+ECHO.
+START CMD /C %CMDSTR%
+SET CMDSTR=
